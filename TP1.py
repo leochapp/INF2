6a6c46cef62963c826ecfd4833b21e4ed520d050
leo <chappuisleopold@gmail.com>
2022-09-19 14:41:17 +0200

    ajout TP1

---
+++ TP1.py
@@ -0,0 +1,45 @@
+from math import *
+
+
+def surface():
+    rayon = float(input("Veuillez rentrer le rayon : "))
+    return pi * (rayon ** 2)
+
+
+def produit():
+    nombre1 = int(input("Veuillez rentrer le premier nombre : "))
+    nombre2 = int(input("Veuillez rentrer le deuxième nombre : "))
+    if nombre1 * nombre2 == 0:
+        return "produit nul"
+    elif nombre1 * nombre2 < 0:
+        return "negatif"
+    else:
+        return "positif"
+
+
+def fact(nombre):
+    fact=1
+    for i in range(1,nombre+1):
+        fact=fact*i
+    print(fact)
+
+
+def bisextile():
+    annee = int(input("Veuillez rentrer une annee : "))
+    if annee % 4 == 0 and annee % 100 != 0 and annee % 400 == 0:
+        print("L'anneée est bisextile ")
+    else:
+        print("L'année n'est pas bisextile ")
+
+
+def coord():
+    x1 = int(input("Veuuillez rentrer la valeur de x1 "))
+    y1 = int(input("Veuillez rentrer la valeur de y1"))
+    x2 = int(input("Veuillez rentrer la valeur de x2"))
+    y2 = int(input("Veuillez rentrer la valeur de y2"))
+
+    print(f"La distance entre ces deux points est {sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)}")
+
+
+def tri(tab):
+    print( sorted(tab))
---
+++ main.py
@@ -0,0 +1,8 @@
+from TP1 import *
+
+
+
+
+
+valeurs = [7,5,4,9,3]
+tri(valeurs)
